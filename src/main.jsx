562450414a62c061600c440fb3000eb2d799c1c4
supersixaxis <appert.nathan@outlook.fr>
2024-04-23 16:04:46 +0200

    base du projet

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
