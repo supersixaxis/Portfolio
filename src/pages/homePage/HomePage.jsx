0e12db60c593510a77484b9b67cbe62c98bf4381
supersixaxis <appert.nathan@outlook.fr>
2024-05-15 16:02:52 +0200

    implémentation d'animations

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,47 +1,44 @@
-import codeImg from '../../assets/background-home.jpg'
-import Card from './component/Card';
-import AboutMe from './component/AboutMe';
-import Typewriter from "typewriter-effect";
 import React, { useState, useEffect } from "react";
-import Loader from '../../components/Loader';
+import Typewriter from "typewriter-effect";
+import { motion } from "framer-motion";
+import Loader from "../../components/Loader";
+import Card from "./component/Card";
+import AboutMe from "./component/AboutMe";
+import codeImg from "../../assets/background-home.jpg";
+
 export default function HomePage() {
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-      // Simule un délai de chargement
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 2000); // 3 secondes
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // 2 secondes
 
-      // Nettoyage du timer lorsqu'il n'est plus nécessaire
-      return () => clearTimeout(timer);
-    }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-    if (isLoading) {
-      return (
-       <Loader />
-      );
-    }
+  if (isLoading) {
+    return <Loader />;
+  }
 
+  const icon = "test";
+  const title1 = "Application sur mesure";
+  const text1 = "Je réalise vos projets selon vos besoins.";
+  const title2 = "SEO";
+  const text2 =
+    "Je met en place une stratégie de référencement pour mettre en avant votre projet.";
+  const title3 = "Application sur mesure";
+  const text3 = "Je réalise vos projets selon vos besoins.";
 
-    let icon = "test"
-    let title1 = "Application sur mesure"
-    let text1 = "Je réalise vos projets selon vos besoins."
-    let icon2 = "test"
-    let title2 = "SEO"
-    let text2 = "Je met en place une stratégie de référencement pour mettre en avant votre projet."
-    let icon3 = "test"
-    let title3 = "Application sur mesure"
-    let text3 = "Je réalise vos projets selon vos besoins."
   return (
-    <div className="bg-color-primaire ">
+    <div className="bg-color-primaire">
       <div className="relative">
         <img
           src={codeImg}
-          alt=""
+          alt="background"
           className="h-auto w-full brightness-50 filter"
         />
-        <div className="bg-color-primaire absolute left-0 top-0 h-full w-full  opacity-70"></div>
+        <div className="bg-color-primaire absolute left-0 top-0 h-full w-full opacity-70"></div>
         <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform text-center">
           <p className="py-4 text-xl font-bold text-white">Bienvenue !</p>
           <h1 className="text-5xl font-bold text-white">
@@ -54,19 +51,39 @@ export default function HomePage() {
             />
           </h1>
           <p className="py-6 text-xl font-bold text-white">
-            Si vous avez besoin contacter moi
+            Si vous avez besoin contactez moi
           </p>
           <button className="rounded-md border bg-blue-600 px-6 py-2 text-xl font-bold text-white hover:border hover:border-blue-500 hover:bg-transparent hover:text-white">
             Contact
           </button>
         </div>
       </div>
-      <div className="flex justify-around">
-        <Card icon={icon} title={title1} text={text1} />
-        <Card icon={icon} title={title2} text={text2} />
-        <Card icon={icon} title={title3} text={text3} />
+      <div className="flex justify-around py-8">
+        {[
+          { icon, title: title1, text: text1 },
+          { icon, title: title2, text: text2 },
+          { icon, title: title3, text: text3 },
+        ].map((card, index) => (
+          <motion.div
+            key={index}
+            className="card-container"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Card icon={card.icon} title={card.title} text={card.text} />
+          </motion.div>
+        ))}
       </div>
-      <AboutMe />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+      >
+        <AboutMe />
+      </motion.div>
     </div>
   );
 }
